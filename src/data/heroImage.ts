52421b6e73936ca7e450c4a6a0c385bf00c45863
speedyhawke <speedyhawke@gmail.com>
2026-09-19 19:53:37 -0230

    Add files via upload

---
+++ src/data/heroImage.ts
@@ -0,0 +1,4 @@
+// Authentic St. John's, Newfoundland & Battery photo from fillthegapnl.ca
+export const ORIGINAL_FILLTHEGAP_IMAGE = "scenic_nl.jpg";
+export const DEFAULT_BATTERY_HERO_IMAGE = ORIGINAL_FILLTHEGAP_IMAGE;
+
