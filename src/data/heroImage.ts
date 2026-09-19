477971ffb2caf324a43f1433cb6db0d38126afe3
speedyhawke <speedyhawke@gmail.com>
2026-09-19 21:08:45 -0230

    Delete src directory

--- src/data/heroImage.ts
+++
@@ -1,4 +0,0 @@
-// Authentic St. John's, Newfoundland & Battery photo from fillthegapnl.ca
-export const ORIGINAL_FILLTHEGAP_IMAGE = "scenic_nl.jpg";
-export const DEFAULT_BATTERY_HERO_IMAGE = ORIGINAL_FILLTHEGAP_IMAGE;
-
